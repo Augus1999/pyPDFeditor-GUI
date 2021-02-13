f5c699753a91173e2d21cca57e705c20366d666e
Phoenix AUGUS <39725660+Augus1999@users.noreply.github.com>
2021-02-13 12:50:32 +0000

    Delete cache_clean.py

--- cache_clean.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# ! Python
-# Author: Nianze A. TAO
-import os
-
-for root, _, files in os.walk('cache'):
-    for name in files:
-        if name.endswith('.pdf'):
-            os.remove(os.path.join(root, name))
