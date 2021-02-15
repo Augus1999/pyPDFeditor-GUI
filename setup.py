3a019aaa5f9c7ea846269c88fe42e05da12d2779
Phoenix AUGUS <39725660+Augus1999@users.noreply.github.com>
2021-02-15 14:36:48 +0000

    update

--- setup.py
+++ setup.py
@@ -10,3 +10,27 @@ except ImportError:
         c = sp.call('pip install -r requirement.txt', shell=True)
         if c == 0:
             break
+
+
+import os
+import pythoncom
+from win32com.shell import shell
+
+
+def set_shortcut():
+    try:
+        filename = os.getcwd()+"\\run.vbs"
+        iconname = os.getcwd()+"\\ico\\pdf icon.ico"
+        lnk_name = os.getcwd()+"\\pdfEditor.lnk"
+        shortcut = pythoncom.CoCreateInstance(shell.CLSID_ShellLink, None,
+                                              pythoncom.CLSCTX_INPROC_SERVER,
+                                              shell.IID_IShellLink)
+        shortcut.SetPath(filename)
+        shortcut.SetWorkingDirectory(os.getcwd())
+        shortcut.SetIconLocation(iconname, 0)
+        shortcut.QueryInterface(pythoncom.IID_IPersistFile).Save(lnk_name, 0)
+    except Exception as e:
+        print(e.args)
+
+
+set_shortcut()
