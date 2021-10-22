666743ffb269a5a6d7073d571dc338049821c691
SUENO Omozawa <39725660+Augus1999@users.noreply.github.com>
2021-10-22 20:59:05 +0100

    release version 2.0
    2021-10-22

--- pdfEditor.py
+++ pdfEditor.py
@@ -4,6 +4,6 @@ from scripts import __main__, __system__, __version__
 
 
 if __name__ == '__main__':
-    # 8960
-    __main__(__system__, __version__, debug=True)
+    # 8964
+    __main__(__system__, __version__, debug=False)
     # **************** 8 9 6 4 ****************
