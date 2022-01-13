02c91d267ed5d76fd91b62fbadc3e72b80010e7b
SUENO Omozawa <39725660+Augus1999@users.noreply.github.com>
2022-01-13 18:56:14 +0000

    version 2.0.7 upload to pypi

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project is based on PyQt5 and PyMuPDF and tested on Python 3.8.6 & 3.9.7 on Windows 10 & 11.
 
-Current version is v2.0.6 ⌛
+Current version is v2.0.7 ⌛
 
 ## Welcome 🎃🎉
 
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """
 set up the application
 """
+from shutil import rmtree
 from setuptools import setup
 from pypdfeditor_core import __version__
 
@@ -11,7 +12,7 @@ with open('README.md', mode='r', encoding='utf-8') as f:
 
 long_description = long_description.replace(
     '<img src="./screenshots/tab2.png" width="400" alt="tab2 win11"/>',
-    '![tab2 win11](https://github.com/Augus1999/pyPDFeditor-GUI/blob/main/screenshots/tab2.png)',
+    '<img src="https://user-images.githubusercontent.com/39725660/149351062-323c8688-9739-4072-b070-d6c15bd7dbd8.png" width="400" alt="tab2 win11"/>',
 )
 
 setup(
@@ -31,3 +32,4 @@ setup(
     install_requires=["PyMuPDF>=1.19.2", "PyQt5>=5.15.4"],
     url="https://github.com/Augus1999/pyPDFeditor-GUI",
 )
+rmtree('pyPDFeditor_GUI.egg-info')  # remove egg-info dir
